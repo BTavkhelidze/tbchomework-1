11f5493372ef80d293f3976e8f2c6dff554e8415
BTavkhelidze <beqatavkhelidze4@gmail.com>
2024-09-26 13:27:58 +0400

    feature html
    adding card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import './Card.css';
 
-const Card = () => {
+const Card = ({ title, imgUrl, description }) => {
   return (
     <div className='card1 card'>
-      <h1 className='title'>Camera</h1>
-      <img
-        src='https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-madebymath-90946.jpg&fm=jpg'
-        alt='Blink Video Doorbell'
-      />
-      <p className='description'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ea
-        necessitatibus saepe neque a ab eos adipisci animi? Vel est mollitia
-        veniam dignissimos explicabo! Fugit non quis possimus labore aliquid
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ea
-        necessitatibus saepe neque a ab eos adipisci animi? Vel est mollitia
-        veniam dignissimos explicabo! Fugit non quis possimus labore aliquid
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ea
-        necessitatibus saepe neque a ab eos adipisci animi? Vel est mollitia
-        veniam dignissimos explicabo! Fugit non quis possimus labore aliquid
-      </p>
-      <button></button>
+      <h1 className='title'>{title}</h1>
+      <div className='image_wrapper'>
+        <img src={imgUrl} alt='Blink Video Doorbell' />
+      </div>
+      <p className='description'>{description}</p>
+      <button type='button'>Add to Cart</button>
     </div>
   );
 };
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -5,25 +5,32 @@ import Card from '../Card/Card';
 const Content = () => {
   return (
     <main className='content_container'>
-      <Card />
-      <div className='card2 card'>
-        <h1>Card 2</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est magni
-          laboriosam, aspernatur cumque minus modi fugiat aut omnis placeat
-          ipsam, harum sapiente porro, cupiditate dolor quibusdam mollitia vitae
-          nam ea.
-        </p>
-      </div>
-      <div className='card3 card'>
-        <h1>Card 3</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est magni
-          laboriosam, aspernatur cumque minus modi fugiat aut omnis placeat
-          ipsam, harum sapiente porro, cupiditate dolor quibusdam mollitia vitae
-          nam ea.
-        </p>
-      </div>
+      <Card
+        title='Camera'
+        imgUrl='https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-madebymath-90946.jpg&fm=jpg'
+        description=' Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ea
+        necessitatibus saepe neque a ab eos adipisci animi Vel est mollitia
+        veniam dignissimos explicabo! Fugit non quis possimus labore aliquid
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ea'
+      />
+      <Card
+        title={'Smart Watch'}
+        imgUrl={
+          'https://images.unsplash.com/photo-1523275335684-37898b6baf30?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D'
+        }
+        description={
+          ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ea     Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ea'
+        }
+      />
+      <Card
+        title={'Organic Beauty Products'}
+        imgUrl={
+          'https://media.istockphoto.com/id/1320934166/photo/cosmetic-skin-care-products-on-green-leaves.jpg?s=612x612&w=0&k=20&c=X4pwnTaBzXHDOGZlcdJdlKxmYd__61xboHVIiR5JMIk='
+        }
+        description={
+          ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ea     Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ea'
+        }
+      />
     </main>
   );
 };
